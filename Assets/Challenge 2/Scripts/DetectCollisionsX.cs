20244b46d4ac52b5e738ba8e5c8be07555195297
Alejandro David Sánchez Marrero <72500168+AlejandroDVDSM@users.noreply.github.com>
2022-09-05 12:34:15 +0100

    Added UI

--- Assets/Challenge 2/Scripts/DetectCollisionsX.cs
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisionsX : MonoBehaviour
 {
+    
+    private ScoreDisplayX scoreDisplayX;
+    
+    public void Start()
+    {
+        scoreDisplayX = GameObject.Find("ScoreText").GetComponent<ScoreDisplayX>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        scoreDisplayX.score++;
     }
 }
